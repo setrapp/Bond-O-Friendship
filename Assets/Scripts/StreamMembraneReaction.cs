1e842b3858cd21ebfa6934c922ccb72b28bc1238
unknown <set9069@NM2550-7.main.ad.rit.edu>
2015-04-09 00:43:34 -0400

    Working on bonding ceremony. Need to hook up membrane reaction to happen when both sides are filled.

---
+++ Assets/Scripts/StreamMembraneReaction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StreamMembraneReaction : StreamReaction {
+
+	public MembraneWall membraneWall;
+	private Membrane createdMembrane;
+
+	void Awake()
+	{
+		membraneWall.createOnStart = false;
+	}
+
+	public override void React(float actionRate)
+	{
+		if (reactionProgress < 1)
+		{
+			base.React(actionRate);
+
+			if (reactionProgress >= 1)
+			{
+				if (createdMembrane == null)
+				{
+					createdMembrane = membraneWall.CreateWall();
+				}
+			}
+		}
+	}
+}
